3ceb4c6c43dd402acc1819d5afed7e6086965a62
phillip0726 <37935285+phillip0726@users.noreply.github.com>
2019-05-13 15:35:40 +0900

    add comment

--- crawler/naver_crawler.py
+++ crawler/naver_crawler.py
@@ -29,22 +29,31 @@ def crawling():
     driver = wd.Chrome('./tool/chromedriver.exe')
     driver.maximize_window()
 
+    # 블로그 제목에 해당하는 css selector -> 중간에 BLOG_NUM에 따라 계속 바뀜
     _const_title_selector = '#content > section > div.area_list_search > div:nth-child(%%BLOG_NUM%%) > div > div.info_post > div > a.desc_inner > strong > span.title'
+
+    # 블로그 미리보기 내용에 해당하는 css selector -> 중간에 BLOG_NUM에 따라 계속 바뀜
     _const_content_selector = '#content > section > div.area_list_search > div:nth-child(%%BLOG_NUM%%) > div > div.info_post > div > a.text'
 
     temporary_storage_num = 1
-    for PAGE_NUM in range(page_num + 1):
+    for PAGE_NUM in range(1, page_num + 1):
         print(f'Start crawling page {PAGE_NUM}')
+        
+        # page 번호에 해당하는 url을 계산
         link = NAVER_URL.replace('%%PAGE_NUM%%', str(PAGE_NUM)) + keyword
         driver.get(link)
+        
+        # page 로딩 시간 기다림
         time.sleep(2)
         for BLOG_NUM in range(1, BLOG_COUNT_PER_PAGE + 1):
             title_selector = _const_title_selector.replace('%%BLOG_NUM%%',str(BLOG_NUM))
             content_selector = _const_content_selector.replace('%%BLOG_NUM%%', str(BLOG_NUM))
-            
+
+            # 제목과 내용을 가져옴
             title  = driver.find_element_by_css_selector(title_selector).text
             content = driver.find_element_by_css_selector(content_selector).text
 
+            # 특수기호 없애는 작업
             for idx in range(len(title)):
                 if not ((0 <= ord(title[idx]) < 128) or (0xac00 <= ord(title[idx]) <= 0xd7af)):
                     title = title.replace(title[idx], ' ')
@@ -55,9 +64,10 @@ def crawling():
 
             data.append([title, content])
 
+        # 컴퓨터 메모리 때문에 100 page씩 데이터를 저장하고 버퍼를 지운다.
         if temporary_storage_num % 100 == 0:
-            dataframe = pd.DataFrame(data, columns=["title", "url"])
-            dataframe.to_csv('./naver_comment.csv', mode='a', encoding='cp949')
+            dataframe = pd.DataFrame(data, columns=["title", "content"])
+            dataframe.to_csv('../data/naver_comment.csv', mode='a', encoding='cp949')
             data = []
             
         temporary_storage_num += 1
@@ -67,7 +77,7 @@ def crawling():
     print('Finish crawling')
     print('The data is being written to the csv file.')
     dataframe = pd.DataFrame(data, columns=["title", "url"])
-    dataframe.to_csv('./naver_comment.csv', mode='a', encoding='cp949')       
+    dataframe.to_csv('../data/naver_comment.csv', mode='a', encoding='cp949')       
     print('Finish working')
 
     
